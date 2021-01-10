9639f5ef5964dc8cc4e2a4cb4d680198f6766542
Andrey141414 <andrusha.vinokurov@gmail.com>
2021-01-10 10:22:38 +0600

    First c Sharp PO

---
+++ Chess_figure.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace A
+{
+    public class Chess_figure
+    {
+        private String[] NAMES = { "Пешка", "Конь", "Слон", "Ладья", "Ферзь" };
+        private String name;
+
+        public int value;
+        public bool pawn;
+
+
+        public void Read()
+        {
+
+            int vibor;
+            Console.WriteLine("Название фигуры");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i + 1}  {NAMES[i]}");
+              
+            }
+
+
+            vibor = Convert.ToInt32(Console.ReadLine());
+
+            name = NAMES[vibor - 1];
+            switch (vibor)
+            {
+                case 1: { value = 1; pawn = true; } break;
+                case 2: { value = 3; } break;
+                case 3: { value = 3; } break;
+                case 4: { value = 5; } break;
+                case 5: { value = 9; } break;
+            }
+
+
+        }
+
+
+        public void Init(int val, String nam)
+        {
+            value = val;
+            name = nam;
+            if (name == NAMES[0])
+                pawn = true;
+            else
+                pawn = false;
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"{name} Ценность {value}");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            String[] NAMES = { "Пешка", "Конь", "Слон", "Ладья", "Ферзь" };
+            Chess_figure[] MAS = new Chess_figure[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                MAS[i] = new Chess_figure();
+                switch (i % 5)
+                {
+                    case 0: { MAS[i].Init(1, NAMES[i % 5]); }; break;
+                    case 1: { MAS[i].Init(3, NAMES[i % 5]); }; break;
+                    case 2: { MAS[i].Init(3, NAMES[i % 5]); }; break;
+                    case 3: { MAS[i].Init(5, NAMES[i % 5]); }; break;
+                    case 4: { MAS[i].Init(9, NAMES[i % 5]); }; break;
+                }
+
+            }
+
+            Chess_Player Andrey = new Chess_Player();
+            Chess_Player Magnus = new Chess_Player();
+            Magnus.playerInit("Magnus", 10, MAS, 1);
+            Andrey.playerRead();
+
+            Andrey.playerDisplay();
+            Console.Clear();
+            Console.ReadKey();
+            Magnus.playerDisplay();
+            Andrey.playerCompare(Magnus);
+
+        }
+    }
+}
